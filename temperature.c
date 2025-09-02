7710c879c34cbcc1b66938961e62e66202978b3d
kab33raa <kabeerbhowmik@gmail.com>
2025-09-02 14:25:12 +0530

    Create temperature.c

---
+++ temperature.c
@@ -0,0 +1,12 @@
+//3rd
+#include <stdio.h>
+
+void main() {
+    float c, f;
+    printf("Enter temp in C: ");
+    scanf("%f", &c);
+    f = (9.0 / 5.0) * c + 32;
+    printf("C: %f\n", c);
+    printf("F: %f\n", f);
+
+}
